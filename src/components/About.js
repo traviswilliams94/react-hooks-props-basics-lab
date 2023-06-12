16f6dc546478e0e1b6738195eb87a98778f4d503
Travis <travis.williams94@gmail.com>
2023-06-12 09:39:23 -0600

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio ? props.bio : null }</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,12 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
-function App() {
+function App(props) {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
